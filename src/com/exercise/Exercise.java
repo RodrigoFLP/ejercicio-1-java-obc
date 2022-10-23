9acd6267f48d9e68e833426be9d5a7bddf5e6f8d
Rodrigo Lopez <rodrigolp0521@gmail.com>
2022-10-23 14:14:45 -0600

    initial commit

---
+++ src/com/exercise/Exercise.java
@@ -0,0 +1,39 @@
+package com.exercise;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+
+        //numeric
+        byte number1 = 1;
+        System.out.println(number1);
+
+        int number2 = 10;
+        System.out.println(number2);
+
+        short number3 = 2;
+        System.out.println(number3);
+
+        long number4 = 5444;
+        System.out.println(number4);
+
+
+        //Decimals
+        float variable1 = 3.2f;
+        System.out.println(variable1);
+
+        double variable2 = 43.4d;
+        System.out.println(variable2);
+
+
+        //boolean
+        boolean bool = false;
+        System.out.println(bool);
+
+        //String
+        String stringExample = "this is an example";
+        System.out.println(stringExample);
+
+
+    }
+}
